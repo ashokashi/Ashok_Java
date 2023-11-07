337b61689b958a98dd80a597aa3207126f8c346e
ashok <ashokashi1997@gmail.com>
2023-11-07 18:30:45 +0530

    LogOut

---
+++ src/com/ashok/Palindrome.java
@@ -0,0 +1,12 @@
+package com.ashok;
+
+public class Palindrome
+{
+
+	
+	public static void main(String[] args)
+	{
+		System.out.println("\n Program to check weather the given string is palindrome or not");
+	}
+
+}
